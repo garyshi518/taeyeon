58a117eec7c83d76110ab14127c5e18a13b9ee7d
Gary Shi <garyshi518@gmail.com>
2019-12-12 10:40:43 +0800

    使用者登入登出

--- message/urls.py
+++ message/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', MessageList.as_view()),
-    path('<int:pk>/', MessageDetail.as_view()),
-    path('create/', MessageCreate.as_view()),
+    path('', MessageList.as_view(), name='msg_list'),
+    path('<int:pk>/', MessageDetail.as_view(), name='msg_view'),
+    path('create/', MessageCreate.as_view(), name='msg_create'),
+    path('delete/<int:pk>/', MessageDelete.as_view(), name='msg_delete'),
+    path('user/', include('django.contrib.auth.urls')),
 ]
